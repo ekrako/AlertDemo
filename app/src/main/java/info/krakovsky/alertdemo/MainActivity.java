3f521009cd1a151f2c93041d3bc32608dd84b9fd
Eran Krakovsky <ekrako@gmail.com>
2019-01-26 18:58:39 +0200

    Initial commit

---
+++ app/src/main/java/info/krakovsky/alertdemo/MainActivity.java
@@ -0,0 +1,28 @@
+package info.krakovsky.alertdemo;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are You Sure!?")
+                .setMessage("Do you definitely want to this?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Yes Was click", Toast.LENGTH_SHORT).show();
+                    }
+                }).setNegativeButton("No",null)
+                .show();
+    }
+}
